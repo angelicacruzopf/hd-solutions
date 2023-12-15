06eddd238a8a663ff262613da44911ce422cc970
angelicacruzopf <angelica.cruz@oneparkfinancial.com>
2023-12-15 11:50:42 -0600

    Cambio d etexto Departamentos

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -4,7 +4,7 @@ export default function Team() {
     return (
         <section class="flex justify-center bg-secondary text-white">
             <div class="flex flex-col items-center w-9/12 pt-16 pb-24">
-                <h2 class="font-bold text-center mb-10 text-5xl">Team</h2>
+                <h2 class="font-bold text-center mb-10 text-5xl">Departamentos</h2>
                 <div class="flex flex-wrap justify-center gap-y-20 h-full">
                     <TeamCard />
                 </div>
