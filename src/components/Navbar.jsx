1cd09182c0c4f7a43455c323e9e0c84310abfb51
angelicacruzopf <angelica.cruz@oneparkfinancial.com>
2023-12-15 11:45:36 -0600

    Banner promocional

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,16 +6,16 @@ import {ReactComponent as Logo} from '../assets/img/hs-logo-full.svg';
 export default function Navbar() {
 
     return (
-        <nav class="flex flex-row justify-between items-center text-sm bg-white fixed z-50 w-full shadow-md">
+        <nav class="flex flex-row justify-between items-center text-sm bg-white fixed z-50 w-full shadow-md top-0">
             <div class="m-3 text-xs">
                 <Logo />
             </div>  
             <div class='flex flex-row grow justify-center'>
-                <ul class='navbar flex flex-row grow justify-evenly font-medium text-lighterGray'>
+                <ul class='navbar flex flex-row grow justify-evenly font-medium text-lighterGray text-center'>
                     <li><Link to="/">Inicio</Link></li>
                     <li><Link to="/Productos">Productos</Link></li>
                     <li><Link to="/Sobre-nosotros">Sobre nosotros</Link></li>
-                    <li><Link to="/Departamentos">Departamentgit os</Link></li>
+                    <li><Link to="/Departamentos">Departamentos</Link></li>
                     <li><Link to="/Contacto">Contacto</Link></li>
                     <li><Link to="/Sucursales">Sucursales </Link></li>
                     <li><Link to="/Horarios">Horarios</Link></li> 
