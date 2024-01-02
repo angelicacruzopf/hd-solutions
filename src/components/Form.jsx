a838cfe48102c8e509e5cd4df11c8beb7d5546e8
angelicacruzopf <angelica.cruz@oneparkfinancial.com>
2024-01-02 09:00:31 -0600

    Validadción sencilla form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,24 +3,76 @@
 
 export default function Form() {
     return(
-        <form class="flex flex-col gap-4 h-full w-full">
-            <div class="flex flex-col md:flex-row w-full gap-4">
-                <div>
-                    <input type="text" name="firstName" id="firstName"  placeholder="Nombres" class="placeholder:text-mediumGray"/>
+        <form class="flex flex-col gap-4 h-full text-neitralGray">
+            <div class="flex flex-col md:flex-row gap-4 w-full">
+                <div className="field md:w-1/2">
+                    <input type="text" name="firstName" id="firstName" 
+                    required
+                    maxlength="32"
+                    pattern="[A-Za-z\s]{1,32}" class="placeholder:text-mediumGray
+                    invalid:border-pink-500 invalid:text-pink-600"/>
+                    <label
+                    htmlFor="firstName"
+                    title="Nombres"
+                    data-title="Nombres"
+                    ></label>
+                    <span class="error">
+                    Este campo no puede estar vacío, e.g. John
+                    </span>
                 </div>
-                <div>
-                    <input type="text" name="lastName" id="lastName"  placeholder="Apellidos" class="placeholder:text-mediumGray"/>
+                <div  className="field md:w-1/2">
+                    <input type="text" name="lastName" id="lastName"  maxlength="32"
+                    pattern="[A-Za-z]{1,32}"
+                     required class="placeholder:text-mediumGray invalid:border-pink-500 invalid:text-pink-600"/>
+                     <label
+                    htmlFor="lastName"
+                    title="Apellidos"
+                    data-title="Apellidos"
+                    ></label>
+                    <span class="error">Este campo no puede estar vacío, e.g. Doe</span>
                 </div>
+                
             </div>
             
-            <div class="col-span-2">
-                <input type="email" name="email" id="email"  placeholder="Correo electrónico" class=" placeholder:text-mediumGray"/>
+            <div class="col-span-2 field">
+                <input type="email" name="email" id="email" class=" placeholder:text-mediumGray invalid:border-pink-500 invalid:text-pink-600"
+                pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$"
+                required/>
+                <label
+                htmlFor="email"
+                title="Correo electrónico"
+                data-title="Correo electrónico"
+                className="peer-invalid:text-pink-600"
+            ></label>
+            <span class="error">
+                Este campo no puede estar vacío, e.g. correo@ejemplo.com
+            </span>
             </div>
-            <div class="col-span-2">
-                <input type="tel" name="phone" id="phone"  placeholder="Número telefónico" class=" placeholder:text-mediumGray"/>
+
+
+            <div class="col-span-2 field">
+                <input type="tel" name="phone" id="phone" class=" placeholder:text-mediumGray focus:invalid:border-pink-500 invalid:text-pink-600"
+                pattern="^\+[1-9]{1}[0-9]{3,14}$"
+                required/>
+
+            <label
+                htmlFor="phone"
+                title="Número telefónico"
+                data-title="Número telefónico"
+            ></label>
+            <span class="error">
+                Este campo no puede estar vacío, e.g. +27 123 4567
+            </span>
+                
             </div>
-            <div class="col-span-2 row-span-6 h-full">
-                <textarea name="message" id="message" placeholder="Escribe un mensaje" class="h-full placeholder:pt-2 placeholder:text-mediumGray"></textarea>
+            <div class="col-span-2 row-span-6 h-full field">
+                <textarea name="message" id="message" required class="h-full placeholder:pt-2 placeholder:text-mediumGray focus:invalid:border-pink-500 invalid:text-pink-600"></textarea>
+
+                <label
+                htmlFor="message"
+                title="Escribe un mensaje"
+                data-title="Escribe un mensaje"
+                ></label>
             </div>           
         </form>
     )
